a2afe8414f3fe84022555de3044778ce8336e094
Marco <ocram.marco@gmail.com>
2015-07-27 00:30:46 -0400

    Created test connection to mongoDB

--- MongoDB Test/Controllers/HomeController.cs
+++ MongoDB Test/Controllers/HomeController.cs
@@ -1,16 +1,80 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB_Test.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 
 namespace MongoDB_Test.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IMongoDatabase database;
+        public MongoClient client;
+        public HomeController()
         {
-            return View();
+            client = new MongoClient(Settings.Default.MongoDBConnection);
+            database = client.GetDatabase(Settings.Default.MongoFileDB);
+        }
+        public async Task<ActionResult> Index()
+        {
+//            var document = new BsonDocument
+//{
+//    { "address" , new BsonDocument
+//        {
+//            { "street", "2 Avenue" },
+//            { "zipcode", "10075" },
+//            { "building", "1480" },
+//            { "coord", new BsonArray { 73.9557413, 40.7720266 } }
+//        }
+//    },
+//    { "borough", "Manhattan" },
+//    { "cuisine", "Italian" },
+//    { "grades", new BsonArray
+//        {
+//            new BsonDocument
+//            {
+//                { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
+//                { "grade", "A" },
+//                { "score", 11 }
+//            },
+//            new BsonDocument
+//            {
+//                { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
+//                { "grade", "B" },
+//                { "score", 17 }
+//            }
+//        }
+//    },
+//    { "name", "Vella" },
+//    { "restaurant_id", "41704620" }
+//};
+
+//            var collection = database.GetCollection<BsonDocument>("restaurants");
+//            await collection.InsertOneAsync(document);
+            var collection = database.GetCollection<BsonDocument>("restaurants");
+            var filter = new BsonDocument();
+            var count = 0;
+            
+            var result = await collection.Find(filter).ToListAsync();
+            //using (var cursor = await collection.FindAsync(filter))
+            //{
+                
+            //    while (await cursor.MoveNextAsync())
+            //    {
+
+            //        var batch = cursor.Current;
+            //        foreach (var document in batch)
+            //        {
+            //            // process document
+            //            result.Add(document);
+            //        }
+            //    }
+            //}
+            return Json(result.ToJson(), JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
---
+++ MongoDB Test/Controllers/MongoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MongoDB_Test.Controllers
+{
+    public class MongoController : Controller
+    {
+        // GET: Mongo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Mongo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Mongo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mongo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mongo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Mongo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mongo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Mongo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
